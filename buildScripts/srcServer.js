1f8abadfe67331863c0e1bff22057e284edd31bb
Lyndon Hills <lyndon@tenegi.com>
2019-06-03 20:35:16 +0100

    Added http calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+{"id":1, "firstName":"Bob", "lastName":"Smith","email":"bob@gmail.com"},
+{"id":2, "firstName":"Lyndon", "lastName":"Hills","email":"john@tenegi.com"},
+{"id":3, "firstName":"John", "lastName":"Jones","email":"jj@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if(err){
     console.log(err);
